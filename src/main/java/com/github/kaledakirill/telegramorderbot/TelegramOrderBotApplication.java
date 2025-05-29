805bd51e3112d3bbb957b9e6061cb3092a761588
Kirill <kirill.kaleda16@gmail.com>
2025-05-29 16:16:43 +0300

    Initial Commit

---
+++ src/main/java/com/github/kaledakirill/telegramorderbot/TelegramOrderBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaledakirill.telegramorderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramOrderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramOrderBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kaledakirill/telegramorderbot/TelegramOrderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kaledakirill.telegramorderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramOrderBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
